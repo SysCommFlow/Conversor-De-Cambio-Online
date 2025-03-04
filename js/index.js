9fc7ea0c1ffa61e6128a1917861e55c18d364110
SysCommFlow <syscommflow@gmail.com>
2025-03-04 08:01:15 +0000

    Organizamos a aplicação com novas funcionalinadades

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-function Somar(numberOne, numberTwo) {
-    document.write("A soma é: "+numberOne+numberTwo);
-}
-
-Somar(7, 9);
+document.addEventListener('DOMContentLoaded', () => {
+    const converter = document.getElementById('converter');
+    converter.addEventListener('click', () => {
+        const valorEmKwanza = document.getElementById('valorEmKwanza');
+        if (valorEmKwanza.value == null || valorEmKwanza.value == '') {
+            const convertionModal = new bootstrap.Modal(document.getElementById('convertionModal'));
+            document.getElementById('aviso').textContent = 'Prencha com valor em kwanza para converter';
+            convertionModal.show();
+        }else{
+            //const sifrao = "$";
+            document.getElementById('valorEmDolar').value = valorEmKwanza.value;
+            document.querySelector('#mostrarResultado').textContent = "Resultado: "+valorEmKwanza.value + " Dolares";
+        }
+    });
+});
